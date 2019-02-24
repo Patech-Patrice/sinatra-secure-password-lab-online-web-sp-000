e8b146029377b1f44a50ef4c02c1e913d2325534
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-24 15:22:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,72 +2,66 @@ require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :views, "app/views"
-    enable :sessions
-    set :session_secret, "password_security"
-  end
+	configure do
+		set :views, "app/views"
+		enable :sessions
+		set :session_secret, "password_security"
+	end
 
-  get "/" do
-    erb :index
-  end
+	get "/" do
+		erb :index
+	end
 
-  get "/signup" do
-    erb :signup
-  end
+	get "/signup" do
+		erb :signup
+	end
 
-  post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-     user.save ? redirect '/login' : redirect '/failure'
-   end
+	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
 
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
+		user.save ? redirect "/login" : redirect "/failure"
+	end
 
+	get "/login" do
+		erb :login
+	end
 
-  get "/login" do
-    erb :login
-  end
+	post "/login" do
+		user = User.find_by(:username => params[:username])
 
-  post "/login" do
-    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect "/success"
+		else
+		  redirect "/failure"
+		end
+	end
 
-		 if user && user.authenticate(params[:password])
-     session[:user_id] = user.id
-     redirect "/success"
-   else
-     redirect "/failure"
-   end
-end
+	get "/success" do
+		if logged_in?
+			erb :success
+		else
+			redirect "/login"
+		end
+	end
 
-    get '/success' do
-      if logged_in?
-          erb :success
-        else
-          redirect '/login'
-        end
-      end
+	get "/failure" do
+		erb :failure
+	end
 
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
 
-    get "/failure" do
-     erb :failure
-    end
+	helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
 
-   get "/logout" do
-    session.clear
-    redirect "/"
-   end
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 
 end
