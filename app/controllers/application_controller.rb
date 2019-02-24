1f7b4f2f147f9ea632865bf5888900b4bd777c00
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-24 15:07:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ end
 
 		 if user && user.authenticate(params[:password])
      session[:user_id] = user.id
-     redirect "/success"
+     redirect "/account"
    else
      redirect "/failure"
    end
